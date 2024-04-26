7c6c154174edc7b7ae7e59c26146e8870fc92b79
MuggyBuggy <robertadambuggy@gmail.com>
2024-04-26 12:31:32 +0000

    fibonacchi using recursive functions

--- main.cpp
+++ main.cpp
@@ -5,16 +5,24 @@ int t2 = 1;
 int next_term = t1 + t2;
 int max_terms = 8;
 
+int fib (int n){
+    //n is the nth term result
+
+    if (n==0||n ==1 )
+        return (n);
+    else
+        return (fib(n-1) + fib(n-2));
+
+}
 
 int main()
 {
-    printf("Hello, lab program 1 \r \n");
+    
+    printf("Hello, lab program 1  using recursive functions \r \n");
     printf("Fibonacchi sequence: %d, %d, ", t1, t2);
-for (int i=2; i<= max_terms; i++){
-    printf("%d, ", next_term);
-    t1 = t2;
-    t2 = next_term;
-    next_term = t1 + t2;
+for (int i=0; i<= max_terms; i++){
+    printf("%d, ", fib(i));
+    
 }
 printf("\r \n");
     while (true) {
