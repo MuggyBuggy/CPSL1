921e296f82b3d6770688a2960911beb59bfa4897
MuggyBuggy <robertadambuggy@gmail.com>
2024-04-26 12:16:04 +0000

    fibonacchi series working with loops

--- main.cpp
+++ main.cpp
@@ -1,8 +1,22 @@
 #include "mbed.h"
-//a second commit
-// main() runs in its own thread in the OS
+
+int t1 = 0;
+int t2 = 1;
+int next_term = t1 + t2;
+int max_terms = 8;
+
+
 int main()
 {
+    printf("Hello, lab program 1 \r \n");
+    printf("Fibonacchi sequence: %d, %d, ", t1, t2);
+for (int i=2; i<= max_terms; i++){
+    printf("%d, ", next_term);
+    t1 = t2;
+    t2 = next_term;
+    next_term = t1 + t2;
+}
+printf("\r \n");
     while (true) {
 
     }
